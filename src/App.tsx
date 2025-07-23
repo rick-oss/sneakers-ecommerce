16fcbff0987cefda8ed14a3ca1cf0a7043d55d90
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-07-23 19:48:18 -0300

    init: primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import imageProduct1 from "./assets/images/image-product-1.jpg";
+
+import Header from "./components/layout/Header";
+import ProductGallery from "./components/layout/ProductGallery";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="app">
+        <ProductGallery images={imageProduct1} />
+      </main>
+    </>
+  );
+}
+
+export default App;
