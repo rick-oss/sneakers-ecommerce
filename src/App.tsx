dd0a93c0ce8871d45119bfa3656dc67fb700f92e
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-07-24 19:25:14 -0300

    chore: importa o componente de informações de produto

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import imageProduct1 from "./assets/images/image-product-1.jpg";
 
 import Header from "./components/layout/Header";
 import ProductGallery from "./components/layout/ProductGallery";
+import ProductInfo from "./components/layout/ProductInfo";
 
 function App() {
   return (
