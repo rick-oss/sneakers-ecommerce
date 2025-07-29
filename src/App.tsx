38aa346b864d4ebba6981c90d43d725b40e31c05
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-07-29 20:39:06 -0300

    style: adiciona estilos temporarios para montagem do layout mobile

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import AddToCart from "./components/layout/AddToCart";
 
 function App() {
   return (
-    <>
+    <div style={{ width: "100%", maxWidth: "375px", margin: "0 auto" }}>
       <Header />
       <main className="app">
         <ProductGallery images={imageProduct1} />
