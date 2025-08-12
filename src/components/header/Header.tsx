f2bf75515bc703b97ba6689cc7c9767ac7f0e088
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-08-12 20:36:38 -0300

    chore: organiza pastas movendo header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,25 +1,41 @@
+import { useState } from "react";
+import styles from "./Header.module.css";
+
+import DrawerMenu from "../common/DrawerMenu";
+
 import logo from "../../assets/images/logo.svg";
 import iconCart from "../../assets/images/icon-cart.svg";
 import iconMenu from "../../assets/images/icon-menu.svg";
 import imageAvatar from "../../assets/images/image-avatar.png";
 
-import styles from "./Header.module.css";
-
 function Header() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const openDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+    console.log("Drawer toggled!");
+  };
+
+  const closeDrawer = () => {
+    setIsDrawerOpen(false);
+  };
+
   return (
     <header className={styles.header_wrapper}>
       <div className={styles.header_left}>
-        <button>
-          <img src={iconMenu} alt="Menu" />
+        <button aria-label="Menu" onClick={openDrawer}>
+          <img src={iconMenu} alt="" />
         </button>
         <img className={styles.image_logo} src={logo} alt="sneakers logo" />
       </div>
       <div className={styles.header_right}>
-        <button>
-          <img src={iconCart} alt="carrinho" />
+        <button aria-label="Cart">
+          <img src={iconCart} alt="" />
         </button>
         <img className={styles.image_profile} src={imageAvatar} alt="imagem de perfil" />
       </div>
+
+      {isDrawerOpen && <DrawerMenu onclose={closeDrawer}/>}
     </header>
   );
 }
