a08c290652970fd9630861dc0221f9e399fbc968
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-08-16 19:48:10 -0300

    feat: monta o componente q mostra os items adicionados ao carrinho

---
+++ src/components/common/CartDropDown.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import styles from "./CartDropDown.module.css";
+
+function CartDropDown({ isOpen }: { isOpen: boolean }) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Muda a visibilidade apenas quando o componente é aberto
+  // Isso faz com que a transição funcione corretamente
+  useEffect(() => {
+    if (isOpen) {
+      setIsVisible(true);
+    }
+  }, [isOpen]);
+
+  if (!isOpen) return null;
+
+  return (
+    <div className={`${styles.cart_dropdown} ${isVisible ? styles.show : ""}`}>
+      <p>Cart</p>
+      <ul>
+        <li>Your cart is empty.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default CartDropDown;
