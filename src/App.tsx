80f15bf811800ff33bc8ef465be7d33b35ca86f6
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-08-19 21:01:26 -0300

    refactor: move componentes do App.tsx para um componente de page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
 import "./App.css";
 
-import imageProduct1 from "./assets/images/image-product-1.jpg";
-
-import Header from "./components/header/Header";
-import ProductGallery from "./components/layout/ProductGallery";
-import ProductInfo from "./components/layout/ProductInfo";
-import AddToCart from "./components/layout/AddToCart";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
-  return (
-    <div style={{ width: "100%", maxWidth: "375px", margin: "0 auto" }}>
-      <Header />
-      <main className="app">
-        <ProductGallery images={imageProduct1} />
-        <ProductInfo
-          companyName="Sneaker Company"
-          productTitle="Fall Limited Edition Sneakers"
-          productDescription="These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer."
-        />
-      </main>
-      <AddToCart />
-    </div>
-  );
+  return <ProductPage />;
 }
 
 export default App;
