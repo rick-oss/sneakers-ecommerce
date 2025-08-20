d2c9ee290b2ae6eab97e9343c11f18fa52fa0360
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-08-19 21:15:11 -0300

    feat: implementa renderização de preços dinâmicos

--- src/components/layout/AddToCart.tsx
+++ src/components/layout/AddToCart.tsx
@@ -1,8 +1,42 @@
+import { useState, useEffect } from "react";
+
 import styles from "./AddToCart.module.css";
 
 import iconMinus from "../../assets/images/icon-minus.svg";
 import iconPlus from "../../assets/images/icon-plus.svg";
 import iconCart from "../../assets/images/icon-cart.svg";
+import imageProduct1 from "../../assets/images/image-product-1.jpg";
+
+interface AddToCartProps {
+  onAdd: (product: {
+    id: number;
+    image: string;
+    productName: string;
+    productPrice: number;
+    productQuantity: number;
+    totalPrice: number;
+  }) => void;
+}
+
+function AddToCart({ onAdd }: AddToCartProps) {
+  const [quantity, setQuantity] = useState(0);
+  const [price, setPrice] = useState(125);
+  const [originalPrice, setOriginalPrice] = useState(250);
+
+  const handleIncrease = () => {
+    setQuantity((prev) => (prev < 99 ? prev + 1 : prev));
+  };
+
+  const handleDecrease = () => {
+    setQuantity((prev) => (prev != 0 ? prev - 1 : 0));
+  };
+
+  useEffect(() => {
+    if (quantity > 0) {
+      setPrice(125 * quantity);
+      setOriginalPrice(250 * quantity);
+    }
+  }, [quantity]);
 
 function AddToCart() {
   return (
