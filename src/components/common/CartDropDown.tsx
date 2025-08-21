b3403f342b15a0a763f430c2946bb54bf6a59c9b
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-08-21 20:31:24 -0300

    feat: renderiza items adicionados no carrinho

--- src/components/common/CartDropDown.tsx
+++ src/components/common/CartDropDown.tsx
@@ -25,10 +25,31 @@ function CartDropDown({ isOpen, cartItems }: CartDropDownProps) {
 
   return (
     <div className={`${styles.cart_dropdown} ${isVisible ? styles.show : ""}`}>
-      <p>Cart</p>
-      <ul>
-        <li>Your cart is empty.</li>
+      <p className={styles.cart_title}>Cart</p>
+      <ul className={styles.cart_items}>
+        {cartItems.length > 0 ? (
+          cartItems.map((item) => (
+            <li key={item.id}>
+              <article className={styles.item}>
+                <img src={item.image} alt="Imagem do Item" />
+                <div>
+                  <span>{item.productName}</span>
+                  <p>
+                    ${item.productPrice.toFixed(2)} x {item.productQuantity}
+                    <strong>${item.totalPrice.toFixed(2)}</strong>
+                  </p>
+                </div>
+                <button>
+                  <FaTrashCan />
+                </button>
+              </article>
+            </li>
+          ))
+        ) : (
+          <li>Your cart is empty.</li>
+        )}
       </ul>
+      {cartItems.length > 0 && <button className={styles.checkout_button}>Checkout</button>}
     </div>
   );
 }
