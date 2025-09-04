569f1df4f8bf4b27cb23c02461703d292e8dd505
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-09-04 20:49:21 -0300

    refactor: reorganiza código dentro de uma div content para estilização futura

--- src/components/common/CartDropDown.tsx
+++ src/components/common/CartDropDown.tsx
@@ -26,31 +26,35 @@ function CartDropDown({ isOpen, cartItems, onRemove }: CartDropDownProps) {
 
   return (
     <div className={`${styles.cart_dropdown} ${isVisible ? styles.show : ""}`}>
-      <p className={styles.cart_title}>Cart</p>
-      <ul className={styles.cart_items}>
-        {cartItems.length > 0 ? (
-          cartItems.map((item) => (
-            <li key={item.id}>
-              <article className={styles.item}>
-                <img src={item.image} alt="Imagem do Item" />
-                <div>
-                  <span>{item.productName}</span>
-                  <p>
-                    ${item.productPrice.toFixed(2)} x {item.productQuantity}
-                    <strong>${item.totalPrice.toFixed(2)}</strong>
-                  </p>
-                </div>
-                <button onClick={() => onRemove(item.id)}>
-                  <FaTrashCan />
-                </button>
-              </article>
-            </li>
-          ))
-        ) : (
-          <li className={styles.empty_cart}>Your cart is empty.</li>
-        )}
-      </ul>
-      {cartItems.length > 0 && <button className={styles.checkout_button}>Checkout</button>}
+      <div className={styles.cart_dropdown_content}>
+        <p className={styles.cart_title}>Cart</p>
+        <ul className={styles.cart_items}>
+          {cartItems.length > 0 ? (
+            cartItems.map((item) => (
+              <li key={item.id}>
+                <article className={styles.item}>
+                  <div>
+                    <img src={item.image} alt="Imagem do Item" />
+                    <span className={styles.item_details}>
+                      {item.productName}
+                      <p>
+                        ${item.productPrice.toFixed(2)} x {item.productQuantity}
+                        <strong>${item.totalPrice.toFixed(2)}</strong>
+                      </p>
+                    </span>
+                  </div>
+                  <button onClick={() => onRemove(item.id)}>
+                    <FaTrashCan />
+                  </button>
+                </article>
+              </li>
+            ))
+          ) : (
+            <li className={styles.empty_cart}>Your cart is empty.</li>
+          )}
+        </ul>
+        {cartItems.length > 0 && <button className={styles.checkout_button}>Checkout</button>}
+      </div>
     </div>
   );
 }
