f56516e17526c1ab9c10b6412fc19c381a31cbd6
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-09-08 11:29:04 -0300

    feat: adiciona logica para link ativo

--- src/components/common/DesktopNavbar.tsx
+++ src/components/common/DesktopNavbar.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import styles from "./DesktopNavbar.module.css";
 
 interface DesktopNavbarProps {
@@ -5,11 +7,20 @@ interface DesktopNavbarProps {
 }
 
 function DesktopNavbar({ menuLinks }: DesktopNavbarProps) {
+  const [linkActive, setLinkActive] = useState("Collections");
   return (
     <nav className={styles.desktop_navbar}>
       <ul>
         {menuLinks.map((link, index) => (
-          <li key={index}>{link}</li>
+          <li key={index} className={`${styles.nav_link}`}>
+            <a
+              href="#"
+              onClick={() => setLinkActive(link)}
+              className={`${linkActive === link ? styles.link_active : ""}`}
+            >
+              {link}
+            </a>
+          </li>
         ))}
       </ul>
     </nav>
