5ad745266ba1def9f8d2df8d6b84adb85da9ee5e
Erick Nunes <141505095+rick-oss@users.noreply.github.com>
2025-09-14 18:34:44 -0300

    refactor: muda estruturas de dados da galeria de imagens para array de imagens

--- src/components/productGallery/CarouselGallery.tsx
+++ src/components/productGallery/CarouselGallery.tsx
@@ -2,24 +2,19 @@ import styles from "./CarouselGallery.module.css";
 
 import GalleryArrows from "./GalleryArrows";
 
-interface galleryItems {
-  image: string;
-  thumbnail: string;
-}
-
 interface CarouselGalleryProps {
-  gallery: galleryItems[];
+  images: string[];
   currentImageIndex?: number;
   onPrev?: () => void;
   onNext?: () => void;
 }
 
-function CarouselGallery({ gallery, currentImageIndex = 0, onPrev, onNext }: CarouselGalleryProps) {
+function CarouselGallery({ images, currentImageIndex = 0, onPrev, onNext }: CarouselGalleryProps) {
   return (
     <section className={styles.carousel_gallery}>
-      <div className={styles.image_container} style={{ transform: `translateX(-${currentImageIndex * 100}%)` }}>
-        {gallery.map((item, index) => (
-          <img src={item.image} key={index} alt="Imagem do produto" />
+      <div className={styles.carousel_track} style={{ transform: `translateX(-${currentImageIndex * 25}%)` }}>
+        {images.map((image, index) => (
+          <img src={image} key={index} alt="Imagem do produto" className={styles.carousel_image} />
         ))}
       </div>
       <GalleryArrows onPrev={onPrev} onNext={onNext} variant="mobile" />
